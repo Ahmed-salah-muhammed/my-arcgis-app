d4e1fa53638055205c98e0630b25bab34d669bda
Ahmed Salah Abd El-bari <142721848+Ahmed-salah-muhammed@users.noreply.github.com>
2026-06-21 22:24:16 +0300

    Add config.js (Salah MCP)

---
+++ config.js
@@ -0,0 +1,15 @@
+window.APP_CONFIG = {
+  "title": "ArcGIS Web App",
+  "description": "",
+  "portalUrl": "https://www.arcgis.com",
+  "webmapId": "2978ed78b952460187bcc75557c46e26",
+  "layerItemIds": [],
+  "basemap": "topo-vector",
+  "widgets": [
+    "legend",
+    "layerList",
+    "search",
+    "basemapGallery",
+    "home"
+  ]
+};
